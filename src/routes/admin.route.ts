acdcae5a3a2e87f19756a0088a82745be5a41e84
Bibek-batoibhai <bibek.samal@batoibhai.com>
2026-01-23 10:41:18 +0530

    pkg approved by the sub admin done

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -3,6 +3,7 @@ import { adminRegister } from "../controller/admin/admin.controller.js";
 import {
   rootAdminLogin,
   approveAgent,
+  approvePackage
 } from "../controller/root admin/rootAdmin.controller.js";
 import { adminMiddleware } from "../middleware/admin.middleware.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
@@ -24,5 +25,6 @@ adminRouter
 adminRouter.route("/login").post(rootAdminLogin);
 adminRouter.route("/logout").delete(authMiddleware, logout);
 adminRouter.route("/approve-agent").post(adminMiddleware, approveAgent);
+adminRouter.route("/approve-pkg").post(adminMiddleware,approvePackage);
 adminRouter.route("/delete-acc").delete(adminMiddleware, deleteAccout);
 export { adminRouter };
